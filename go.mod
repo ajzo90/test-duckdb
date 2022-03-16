6198aa627f514c6836e9d30e1601f9158fcf96a3
Florian Uekermann <florian@uekermann.me>
2022-03-16 19:10:36 +0100

    replace with arrow test

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/ajzo90/test-duckdb
-
-go 1.16
-
-require github.com/marcboeker/go-duckdb v0.0.0-20220311141255-f3ea197bcd4e
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-DUCK=/Users/christianpersson/repos/duckdb
-
-#CGO_LDFLAGS="-L/Users/christianpersson/repos/duckdb/build/release/src" CGO_CFLAGS="-I/Users/christianpersson/repos/duckdb/src/include" DYLD_LIBRARY_PATH="/Users/christianpersson/repos/duckdb/build/release/src" go get github.com/marcboeker/go-duckdb
- cat users.csv | CGO_LDFLAGS="-L/$DUCK/build/release/src" CGO_CFLAGS="-I/$DUCK/src/include" DYLD_LIBRARY_PATH="/$DUCK/build/release/src" go run main.go
