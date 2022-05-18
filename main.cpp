382c68490b43118ec9ba0aad793ebf0eee7cd959
Florian Uekermann <florian@uekermann.me>
2022-05-18 22:56:13 +0200

    move arrow array stream generation to rust

--- main.cpp
+++ main.cpp
@@ -1,56 +1,10 @@
 #include <stdio.h>
 #include <algorithm>
 #include <iostream>
-#include "arrow/api.h"
-#include "arrow/c/bridge.h"
-#include "arrow/c/abi.h"
 #include "duckdb.hpp"
 
-void abort_not_ok(arrow::Status s) {
-    if (!s.ok()) {
-        s.Abort();
-    }
-}
-
-std::shared_ptr<arrow::Schema> schema() {
-    return arrow::schema({
-                                 arrow::field("a", arrow::int32()),
-                                 arrow::field("b", arrow::int32()),
-                         });
-}
-
-int get_next(struct ArrowArrayStream *self, struct ArrowArray *out) {
-    const size_t batch_size = 3;
-    const size_t total_size = 10;
-    auto progress = size_t(uintptr_t(self->private_data));
-    if (progress >= uintptr_t(total_size)) {
-        out->release = NULLPTR;
-        return 0;
-    }
-    self->private_data = (void *) (uintptr_t(progress + batch_size));
-
-    int64_t this_batch_size = std::min(int64_t(batch_size), int64_t(total_size - progress));
-    printf("NEXT off: %lu, len: %ld\n", progress, this_batch_size);
-
-    arrow::NumericBuilder<arrow::Int32Type> builder;
-    abort_not_ok(builder.Reserve(this_batch_size));
-    for (int32_t i = progress; i < progress + this_batch_size; i++)
-        abort_not_ok(builder.Append(i));
-    auto int32_array = builder.Finish().ValueOrDie();
-
-    auto array_vector = arrow::ArrayVector({int32_array, int32_array});
-    auto struct_array = arrow::StructArray::Make(array_vector, schema()->fields()).ValueOrDie();
-    abort_not_ok(ExportArray(*struct_array, out));
-    return 0;
-}
-
-int get_schema(struct ArrowArrayStream *, struct ArrowSchema *out) {
-    abort_not_ok(ExportSchema(*schema(), out));
-    return 0;
-};
-
 extern "C" {
-    void hello_world();
+    ArrowArrayStream get_arrow_array_stream();
 }
 
 std::unique_ptr<duckdb::ArrowArrayStreamWrapper>
@@ -59,12 +13,8 @@ CreateStream(uintptr_t, std::pair<std::unordered_map<duckdb::idx_t, duckdb::stri
     for (duckdb::string i: project_columns.second)
         printf("%s\n", i.c_str());
 
-    printf("CREATE\n");
     auto stream_wrapper = duckdb::make_unique<duckdb::ArrowArrayStreamWrapper>();
-    stream_wrapper->arrow_array_stream.release = nullptr;
-    stream_wrapper->arrow_array_stream.get_schema = get_schema;
-    stream_wrapper->arrow_array_stream.get_next = get_next;
-    stream_wrapper->arrow_array_stream.private_data = NULLPTR;
+    stream_wrapper->arrow_array_stream = get_arrow_array_stream();
     stream_wrapper->number_of_rows = 10000000000;
     return stream_wrapper;
 }
@@ -73,11 +23,12 @@ int main() {
     duckdb::DuckDB db;
     duckdb::Connection conn{db};
     duckdb::vector<duckdb::Value> params;
-    params.push_back(duckdb::Value::POINTER((uintptr_t) NULLPTR));
+    params.push_back(duckdb::Value::POINTER((uintptr_t) NULL));
     params.push_back(duckdb::Value::POINTER((uintptr_t) &CreateStream));
     params.push_back(duckdb::Value::UBIGINT(1000000));
     auto rel_t = conn.TableFunction("arrow_scan", params)->CreateView("t");
     conn.Query("SET threads TO 1;")->Print();
-    conn.Query("SELECT SUM(u.b) FROM t JOIN t u ON t.b = u.a")->Print();
+    conn.Query("SELECT * FROM t")->Print();
+//    conn.Query("SELECT SUM(u.b) FROM t JOIN t u ON t.b = u.a")->Print();
     return 0;
 }
--- rust_arrow_client/src/lib.rs
+++ rust_arrow_client/src/lib.rs
@@ -1,4 +1,62 @@
+use arrow::array::{ArrayData, ArrayRef, StructArray};
+use arrow::buffer::Buffer;
+use arrow::datatypes::{DataType, Field, Schema, SchemaRef};
+use arrow::error::Result;
+use arrow::ffi_stream::FFI_ArrowArrayStream;
+use arrow::record_batch::{RecordBatch, RecordBatchReader};
+use std::cmp::min;
+
 #[no_mangle]
-pub extern "C" fn hello_world() {
-    println!("Hello World!");
+pub extern "C" fn get_arrow_array_stream() -> FFI_ArrowArrayStream {
+    FFI_ArrowArrayStream::new(Box::new(IteratorImpl::new()))
+}
+
+struct IteratorImpl {
+    cnt: usize,
+    schema: SchemaRef,
+}
+
+impl IteratorImpl {
+    fn new() -> Self {
+        Self {
+            cnt: 10,
+            schema: SchemaRef::new(Schema::new(vec![
+                Field::new("a", DataType::Int32, false),
+                Field::new("b", DataType::Int32, false),
+            ])),
+        }
+    }
+}
+
+impl RecordBatchReader for IteratorImpl {
+    fn schema(&self) -> SchemaRef {
+        self.schema.clone()
+    }
+}
+
+impl Iterator for IteratorImpl {
+    type Item = Result<RecordBatch>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cnt == 0 {
+            return None;
+        }
+        let l = min(self.cnt, 3);
+        let fields: Vec<(Field, ArrayRef)> = self
+            .schema()
+            .fields()
+            .iter()
+            .map(|field| {
+                let buffer = Buffer::from_slice_ref(&[1i32, 2i32, 3i32]);
+                let array_data = ArrayData::builder(field.data_type().clone())
+                    .len(l)
+                    .add_buffer(buffer)
+                    .build()
+                    .unwrap();
+                (field.clone(), ArrayRef::from(array_data))
+            })
+            .collect();
+        self.cnt -= l;
+        Some(Ok(RecordBatch::from(&StructArray::from(fields))))
+    }
 }
